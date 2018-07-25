c36d072c670e5a23ab1b8370984f63f16b61584f
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-07-25 16:48:31 +0700

    pre deploy

--- src/Components/Win/index.js
+++ src/Components/Win/index.js
@@ -1,14 +1,127 @@
 import React, { Component } from 'react'
 import { keyframes } from 'styled-components'
 
-const k = keyframes`
-    from {
-        bottom: 200%;
+const text1 = keyframes`
+    0% {
+        opacity: 1;
     }
-    to {
-        bottom: 50%;
+    20% {
+        opacity: 0;
+    }
+    40% {
+        opacity: 0;
+    }
+    60% {
+        opacity: 0;
+    }
+    80% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 0;
     }
 `
+const text2 = keyframes`
+    0% {
+        opacity: 0;
+    }
+    20% {
+        opacity: 1;
+    }
+    40% {
+        opacity: 0;
+    }
+    60% {
+        opacity: 0;
+    }
+    80% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 0;
+    }
+`
+const text3 = keyframes`
+    0% {
+        opacity: 0;
+    }
+    20% {
+        opacity: 0;
+    }
+    40% {
+        opacity: 1;
+    }
+    60% {
+        opacity: 0;
+    }
+    80% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 0;
+    }
+`
+const text4 = keyframes`
+    0% {
+        opacity: 0;
+    }
+    20% {
+        opacity: 0;
+    }
+    40% {
+        opacity: 0;
+    }
+    60% {
+        opacity: 1;
+    }
+    80% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 0;
+    }
+`
+const text5 = keyframes`
+    0% {
+        opacity: 0;
+    }
+    20% {
+        opacity: 0;
+    }
+    40% {
+        opacity: 0;
+    }
+    60% {
+        opacity: 0;
+    }
+    80% {
+        opacity: 1;
+    }
+    100% {
+        opacity: 0;
+    }
+`
+const text6 = keyframes`
+    0% {
+        opacity: 0;
+    }
+    20% {
+        opacity: 0;
+    }
+    40% {
+        opacity: 0;
+    }
+    60% {
+        opacity: 0;
+    }
+    80% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+`
+
 export class Win extends Component {
   render() {
     return (
@@ -17,10 +130,33 @@ export class Win extends Component {
                     position: 'absolute',
                     bottom: '0',
                     left: '0',
+                    color: 'white',
+                    textShadow: '0px 0px 10px black',
                     background: 'linear-gradient(to bottom, black, rgba(0,0,0,0.2))',
                     }}>
-        <div style={{position: 'absolute', bottom: '50%', left: '50%', animation: `5s ${k}`}}>
-            MOON
+
+        <div style={{opacity: '0', position: 'absolute', bottom: '50%', left: '50%', animation: `36s ${text1}`}}>
+            You cannot shake hands with a clenched fist.
+        </div>
+
+        <div style={{opacity: '0', position: 'absolute', bottom: '50%', left: '50%', animation: `36s ${text2}`}}>
+            Let us sacrifice our today so that our children can have a better tomorrow.
+        </div>
+
+        <div style={{opacity: '0', position: 'absolute', bottom: '50%', left: '50%', animation: `36s ${text3}`}}>
+            When you reach the end of your rope, tie a knot in it and hang on.
+        </div>
+
+        <div style={{opacity: '0', position: 'absolute', bottom: '50%', left: '50%', animation: `36s ${text4}`}}>
+            There is no charm equal to tenderness of heart.
+        </div>
+
+        <div style={{opacity: '0', position: 'absolute', bottom: '50%', left: '50%', animation: `36s ${text5}`}}>
+            JOIN US
+        </div>
+
+        <div style={{opacity: '1', position: 'absolute', bottom: '50%', left: '50%', animation: `36s ${text6}`}}>
+            RABBIT DIGITAL GROUP
         </div>
       </div>
     )
